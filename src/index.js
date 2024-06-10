52f3f45518930745c5923217cbd86ef927cdcd5c
Ekaterina Ivanova <ekaterinaivanova@MacBook-Air-Ekaterina.local>
2024-06-10 11:14:10 +0300

    fix input

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	const hideCheckbox = document.getElementById('hide')
 	const progressBlock = document.querySelector('progress-block')
 
-	valueInput.addEventListener('keypress', function (e) {
-		if (!/[0-9]/.test(e.key)) {
-			e.preventDefault()
+	valueInput.addEventListener('input', function (e) {
+		if (/[0-9]/.test(e.data)) {
+			this.value = Math.min(this.value, 100)
+			progressBlock.setAttribute('value', this.value)
+		} else {
+			this.value = 0
 		}
-	})
-	valueInput.addEventListener('input', function () {
-		this.value = Math.min(this.value, 100)
-		progressBlock.setAttribute('value', this.value)
 	})
 
 	animateCheckbox.addEventListener('change', function () {
